a957ff9f884939aaad82cfa20259b3a908c45609
Enoc Santiago <enocsanti2@gmail.com>
2020-03-18 10:30:47 -0400

    Basic login page, no functionality

--- src/main/webapp/angular/src/app/app.module.ts
+++ src/main/webapp/angular/src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { LoginComponent } from './login/login.component';
+import { LoginService } from './login/login.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    LoginService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
