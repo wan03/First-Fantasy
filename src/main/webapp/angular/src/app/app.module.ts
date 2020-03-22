5f5de0ea664733bc311d3915a6f32c4c0fc31546
wan03 <38962452+wan03@users.noreply.github.com>
2020-03-22 15:12:07 -0400

    Made nav bar and registration page

--- src/main/webapp/angular/src/app/app.module.ts
+++ src/main/webapp/angular/src/app/app.module.ts
@@ -1,30 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-
-import { LoginComponent } from './login/login.component';
-import { LoginService } from './login/login.service';
-import { MainMenuComponent } from './main-menu/main-menu.component';
-
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { AppRoutingModule } from "./app-routing.module";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { AppComponent } from "./app.component";
+import { HttpClientModule } from "@angular/common/http";
+import { LoginComponent } from "./login/login.component";
+import { LoginService } from "./login/login.service";
+import { MainMenuComponent } from "./main-menu/main-menu.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { CarouselComponent } from "./carousel/carousel.component";
+import { RegistrationFormComponent } from "./registration-form/registration-form.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    MainMenuComponent
+    MainMenuComponent,
+    NavbarComponent,
+    CarouselComponent,
+    RegistrationFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule
   ],
-  providers: [
-    LoginService
-  ],
+  providers: [],
+  // LoginService
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
