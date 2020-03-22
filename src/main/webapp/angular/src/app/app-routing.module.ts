6ffc5fde02e7fdead384c01ff015d99aa489e966
wan03 <38962452+wan03@users.noreply.github.com>
2020-03-22 16:25:30 -0400

    Set up home page and routing

--- src/main/webapp/angular/src/app/app-routing.module.ts
+++ src/main/webapp/angular/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from "@angular/router";
 import { MainMenuComponent } from "./main-menu/main-menu.component";
 import { LoginComponent } from "./login/login.component";
 import { RegistrationFormComponent } from "./registration-form/registration-form.component";
+import { HomePageComponent } from "./home-page/home-page.component";
 
 const routes: Routes = [
   { path: "menu", component: MainMenuComponent },
   { path: "login", component: LoginComponent },
-  { path: "register", component: RegistrationFormComponent }
+  { path: "register", component: RegistrationFormComponent },
+  { path: "", component: HomePageComponent, pathMatch: "full" }
 
   // {path: '', redirectTo: '/login', pathMatch: 'full'}
   //{path: '**', redirectTo: '/login', pathMatch: 'full'} // If path doesn't exist, reroute to landing page.
